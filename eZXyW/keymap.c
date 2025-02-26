030c58a76c76ef13096dfeb0a8def38a8f5b87ea
George Skryl <george@Georges-MacBook-Pro-M1.local>
2025-02-26 14:12:25 -0500

    Allow cmd-c/v same-hand chord

--- eZXyW/keymap.c
+++ eZXyW/keymap.c
@@ -344,3 +344,22 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
                             '*', '*',  '*', '*'
     );
 
+bool get_chordal_hold(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record,
+                      uint16_t other_keycode, keyrecord_t* other_record) {
+    // Exceptionally allow some one-handed chords for hotkeys.
+    switch (tap_hold_keycode) {
+        case LGUI_T(KC_D):
+           if (other_keycode == KC_C || other_keycode == KC_V) {
+               return true;
+           }
+           break;
+        // case RCTL_T(KC_SCLN):
+        //    if (other_keycode == KC_N) {
+        //        return true;
+        //    }
+        //    break;
+    }
+    // Otherwise defer to the opposite hands rule.
+    return get_chordal_hold_default(tap_hold_record, other_record);
+}
+
