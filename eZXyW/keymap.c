075a02dbdd3cd6900275c873dccbe1c8e1fa121a
George Skryl <george@Georges-MacBook-Pro-M1.local>
2025-02-25 20:27:49 -0500

    Allow chords on thumb keys

--- eZXyW/keymap.c
+++ eZXyW/keymap.c
@@ -341,6 +341,6 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
-                            'L', 'L',  'R', 'R'
+                            '*', '*',  '*', '*'
     );
 
